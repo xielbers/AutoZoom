29715a6e2482c9b08139e49bb535a6c25edd5a5b
xielbers <salvadorrodrig568@gmail.com>
2024-05-20 16:11:02 -0300

    Zoom

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 (function() {
-    // Establecer la URL de destino
     const zoomUrl = "https://us06web.zoom.us/j/87813278236?pwd=aoMVUXAtPNjbvpXc9fC5LvX6VDufeb.1";
-
-    // Obtener la hora actual
-    const now = new Date();
+    const countdownElement = document.getElementById("countdown");
     
-    // Configurar la hora de redirección (6:50 PM) en la zona horaria de Argentina
-    const targetTime = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 18, 50);
-
-    // Calcular la diferencia de tiempo en milisegundos
-    const timeDifference = targetTime - now;
-
-    // Si la hora de redirección ya pasó para hoy, ajustar para el día siguiente
-    if (timeDifference < 0) {
+    const now = new Date();
+    let targetTime = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 18, 50);
+    
+    if (targetTime - now < 0) {
         targetTime.setDate(targetTime.getDate() + 1);
     }
-
-    // Calcular el nuevo tiempo de diferencia
-    const updatedTimeDifference = targetTime - now;
-
-    // Redirigir cuando se alcance el tiempo especificado
-    setTimeout(() => {
-        window.location.href = zoomUrl;
-    }, updatedTimeDifference);
-
-    console.log(`Serás redirigido a las 6:50pm (Argentina). Tiempo restante: ${updatedTimeDifference} ms`);
+    
+    const updateTimeDifference = () => {
+        const currentTime = new Date();
+        const timeDifference = targetTime - currentTime;
+        
+        if (timeDifference <= 0) {
+            window.location.href = zoomUrl;
+        } else {
+            const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+            const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+            countdownElement.textContent = `${hours}:${minutes}:${seconds}`;
+        }
+    };
+    
+    updateTimeDifference();
+    setInterval(updateTimeDifference, 1000);
 })();
